c5571800e3b4f36afcac3aff5b07adb689a78ed9
U4F_rafael04 <shark05813@sharkcoders.pt>
2026-02-16 20:38:08 +0000

    SlotsIntermediario.py

--- exemplo.py
+++ exemplo.py
@@ -4,24 +4,29 @@ import time
 dinheiro = 1000
 
 def saldo():
+    global dinheiro
     print(f'Saldo Atual: {dinheiro}')
     
     valor_input = input("Valor da aposta (ou '0' para sair): ")
     if valor_input == "0":
         exit()
-    return int(valor_input)
+
+    valor = int(valor_input)
+    if valor > dinheiro:
+        print("Não tens dinheiro suficiente!\n")
+        return None
+
+    return valor
     
 def slot(valor):
     global dinheiro
     opcoes = ['🍒', '💎', '7️⃣']
     
-
     print("A girar...", end=" ")
     for _ in range(15):
         reels_fake = [random.choice(opcoes) for _ in range(3)]
         print(f" {' | '.join(reels_fake)} ", end="\r")
-        time.sleep(0.25)   # mais lento
-
+        time.sleep(0.25)
 
     resultado = [random.choice(opcoes) for _ in range(3)]
     print(f"Resultado: {' | '.join(resultado)} \n")
@@ -33,9 +38,19 @@ def slot(valor):
         print("Perdeste, tenta novamente.\n")
         dinheiro -= valor
 
+    if dinheiro <= 0:
+        print("Ficaste sem dinheiro! O jogo vai recomeçar...\n")
+        dinheiro = 1000
+
+
 while True:
     try:
         valor = saldo()
+
+        if valor is None:
+            continue
+
         slot(valor)
+
     except ValueError:
         print("Por favor, insira um número válido.\n")
